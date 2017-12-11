b6f83a641c3a7afede3dc2f7f7587be2bb541997
THISPC\Linus <00jansson@gmail.com>
2017-12-11 21:43:29 +0100

    Updated the mod for SMAPI 2.2

--- PetEnhancement/Action.cs
+++ PetEnhancement/Action.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PetEnhancement
+namespace PetEnhancements
 {
     public enum Action
     {
--- PetEnhancement/PetActionProvider.cs
+++ PetEnhancement/PetActionProvider.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PetEnhancement
+namespace PetEnhancements
 {
     class PetActionProvider
     {
