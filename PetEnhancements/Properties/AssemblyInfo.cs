7591fc8ff8d363518a4642a756d6ef66c4bcef06
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2019-02-27 22:47:32 -0500

    fix typo (Enhancments → Enhancements)

--- PetEnhancements/Properties/AssemblyInfo.cs
+++ PetEnhancements/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PetEnhancments")]
+[assembly: AssemblyTitle("PetEnhancements")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PetEnhancments")]
+[assembly: AssemblyProduct("PetEnhancements")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
